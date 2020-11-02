869907572792b078bca03792ef1f0dc53574b9e0
Ruben Matamoros <ruben@valiantcreative.net>
2020-11-02 13:56:16 -0500

    shop function

--- assets/js/game.js
+++ assets/js/game.js
@@ -74,40 +74,92 @@ var startGame = function() {
   playerAttack = 10;
   playerMoney = 10;
 
-  for (var i = 0; i < enemyNames.length; i++) {
-    if (playerHealth > 0) {
-      window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+    for(var i = 0; i < enemyNames.length; i++) {
 
-      var pickedEnemyName = enemyNames[i];
+      if (playerHealth > 0) {
+          window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+          var pickedEnemyName = enemyNames[i];
+          enemyHealth = 50;
+          fight(pickedEnemyName);
 
-      enemyHealth = 50;
+          if (playerHealth > 0 && i < enemyNames.length -1) {
 
-      fight(pickedEnemyName);
-    }
-    else {
-      window.alert("You have lost your robot in battle! Game Over!");
-      break;
-    }
-  }
- 
+              var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
+
+              if (storeConfirm) {
+                  shop();
+              }
+          }
+      }
+      else {
+          window.alert("You have lost your robot in battle!  Game Over!");
+          break;
+      }
+    } 
+
   endGame();
 };
 
 var endGame = function() {
+
     if (playerHealth > 0) {
-      window.alert("Great job, you've survived the game!  You now have a score of " + playerMoney + ".");
+        window.alert("Great job, you've survived the game!  You now have a score of " + playerMoney + ".");
     }
     else {
         window.alert("You've lost your robot in battle.");
     }
-
     var playAgainConfirm = window.confirm("Would you like to play again?");
-
     if (playAgainConfirm) {
         startGame();
     }
     else {
+        window.alert("Thank you for playing Robot Gladiators!  Come back soon!");
     }
-  };
+};
 
-  startGame();
+var shop = function() {
+
+  var shopOptionPrompt = window.prompt(
+      "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store?  Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+  );
+
+  switch (shopOptionPrompt) {
+
+      case "refill":
+      case "REFILL":
+          if (playerMoney >= 7) {
+              window.alert("Refilling player's health by 20 for 7 dollars.");
+              playerHealth = playerHealth + 20;
+              playerMoney = playerMoney -7;
+          }
+          else {
+              window.alert("You don't have enough money!");
+          }
+          break;
+
+      case "upgrade":
+      case "UPGRADE":
+          if (playerMoney >= 7) {
+              window.alert("Upgrading player's attack by 6 for 7 dollars.");
+              playerAttack = playerAttack + 6;
+              playerMoney = playerMoney - 7;
+          }
+          else {
+              window.alert("You don't have enough money!");
+          }
+          break;
+
+      case "leave":
+      case "LEAVE":
+          window.alert("Leaving the store.");
+          break;
+
+      default: 
+          window.alert("You did not pick a valid option.  Try again.");
+          shop();
+          break;
+
+  }
+};
+
+startGame(); 
