8bea55b55d699da2eec3256d7623fca9b23497b5
Ruben Matamoros <ruben@valiantcreative.net>
2020-11-04 22:21:10 -0500

    added integer responses in the shop function

--- assets/js/game.js
+++ assets/js/game.js
@@ -28,8 +28,6 @@ var fightOrSkip = function() {
 var fight = function(enemy) {
   while (enemy.health > 0 && playerInfo.health > 0) {
   
-    fightOrSkip();
-
     if (fightOrSkip()) {
       break;
   }
@@ -119,26 +117,28 @@ var startGame = function() {
   var shop = function() {
       
       var shopOptionPrompt = window.prompt(
-          "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store?  Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
       );
-      switch (shopOptionPrompt) {
-          case "refill":
-          case "REFILL":
-              playerInfo.refillHealth();
-              break;
-          case "upgrade":
-          case "UPGRADE":
-              playerInfo.upgradeAttack();
-              break;
-          case "leave":
-          case "LEAVE":
-              window.alert("Leaving the store.");
-              break;
-          default: 
-              window.alert("You did not pick a valid option.  Try again.");
-              shop();
-              break;
-      }
+
+      shopOptionPrompt = parseInt(shopOptionPrompt);
+      
+      // use switch case to carry out action
+        
+        switch (shopOptionPrompt) {
+          case 1:
+            playerInfo.refillHealth();
+            break;
+          case 2:
+            playerInfo.upgradeAttack();
+            break;
+          case 3:
+            window.alert("Leaving the store.");
+            break;
+          default:
+            window.alert("You did not pick a valid option. Try again.");
+            shop();
+            break;
+        }
   };
   var randomNumber = function(min, max) {
       var value = Math.floor(Math.random() * (max - min + 1) + min);
